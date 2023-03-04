66e6c0adc133910cf2d1bd21cc692e5b3c64f227
Matthias Westhues <westhues.matthias@gmail.com>
2023-03-04 14:42:18 +0100

    convert to dates

--- loanpred/eda.py
+++ loanpred/eda.py
@@ -7,3 +7,5 @@ uri = 'https://raw.githubusercontent.com/nateGeorge/manning_random_forest_classi
 df = pd.read_csv(uri, index_col=0)
 df['CREDIT_HISTORY_LENGTH'] = df['CREDIT_HISTORY_LENGTH'].map(add_years_months)
 df['AVERAGE_ACCT_AGE'] = df['AVERAGE_ACCT_AGE'].map(add_years_months)
+df['DATE_OF_BIRTH'] = pd.to_datetime(df['DATE_OF_BIRTH'])
+df['DISBURSAL_DATE'] = pd.to_datetime(df['DISBURSAL_DATE'])
